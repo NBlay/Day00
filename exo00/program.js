50479839ddaab1546edac80f14271ad8e49cf75c
admin <admin@iMac-ITA-04.local>
2021-11-29 13:00:08 +0100

    Exo00UpdatedFinal

--- exo00/program.js
+++ exo00/program.js
@@ -2,10 +2,9 @@ let array = (1, 3, 1, 2);
 let targetSum = 4;
 
 function twoNumberSum(array, targetSum){
-	for(let i = 0; i < array.length; i++){
- 		for(let j = 1; j < array.length; j++){
-  		temp = array[i]+array[j];
- 			if(temp == targetSum){
+	for (let i = 0; i < array.length; i++){
+ 		for (let j = 1; j < array.length; j++){
+ 			if (array[i]+array[j] == targetSum && i != j){
       	return [array[i],array[j]];
     	}
   	}
@@ -13,7 +12,7 @@ function twoNumberSum(array, targetSum){
   return [];
 }
 
-console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 8));
+console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10));
 
 // Merci de ne pas effacer la ligne en dessous.
 exports.twoNumberSum =  twoNumberSum;
