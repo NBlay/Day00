28dc407f031ee721aa2be643a5f017aab2a0ffd1
admin <admin@iMac-ITA-04.local>
2021-11-29 15:42:37 +0100

    Exo02 done

--- exo02/program.js
+++ exo02/program.js
@@ -1,14 +1,16 @@
-let array = [13, 54, 1, 2, 450, 999, 69];
+let array = [1, 1129, 69, 31, 45];
+let arrayFin = [];
 
-function threeLargestNumbers(array){
-  for(i = 0; i < array.length; i++){
-    if(i > i+){
-      return i;
-    }
-    return false;
-  }
-console.log(threeLargestNumbers([13, 54, 1, 2, 450, 999, 69]));
+function threeLargestNumber(array){
+	for(i = 0; i < 3; i++){
+    let max = Math.max(...array);
+    arrayFin.push(max);
+    array.splice(array.indexOf(max), 1);
+	}
+ let sortedArrFin = arrayFin.sort((a, b) => a - b);
+ return sortedArrFin
 }
 
+ console.log(threeLargestNumber(array));
 // Merci de ne pas effacer la ligne en dessous.
 exports.threeLargestNumbers =  threeLargestNumbers;
