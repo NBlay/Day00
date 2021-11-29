d9d72e3815ac10077e37f141fe37e3580ef857d9
admin <admin@iMac-ITA-04.local>
2021-11-29 16:49:44 +0100

    All exos done

---
+++ exo04/program.js
@@ -0,0 +1,8 @@
+function levDist(str1, str2){
+// écrivez votre code ici.
+}
+
+// Merci de ne pas effacer la ligne en dessous.
+exports.levDist =  levDist;
+// Do not edit the line below.
+exports.firstNonRepeatingCharacter = firstNonRepeatingCharacter;
